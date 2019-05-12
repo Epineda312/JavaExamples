7aee75e365f6d1c55c8a430d8cbe3b98f3d317f3
Air-ic <epineda773@protonmail.com>
2019-05-12 18:23:39 -0500

    Java package

---
+++ src/com.airic.practice/Animal.java
@@ -0,0 +1,13 @@
+package com.airic.practice;
+
+public class Animal {
+    //Main static method
+    public static void main(String args[]){
+        getName("Max");
+    }
+
+    //Get Name
+    public static void getName(String name){
+        System.out.println("Animal name is: " + name);
+    }
+}
