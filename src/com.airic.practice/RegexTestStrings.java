af547182fc5bc28b8297d37d76bb760ad96ecd4c
Air-ic <epineda773@protonmail.com>
2019-05-14 03:42:52 -0500

    Regular Expressions

---
+++ src/com.airic.practice/RegexTestStrings.java
@@ -0,0 +1,20 @@
+package com.airic.practice;
+
+public class RegexTestStrings {
+    public static final String EXAMPLE_TEST = "This is my " + "small example";
+
+    //Match any character - .*
+    //Match beginning character - ^
+    //Match ending character - $
+    public static void main(String args[]){
+        System.out.println(EXAMPLE_TEST.matches("\\w.*"));//return true if the string matches
+        String[] splitString = (EXAMPLE_TEST.split("\\s+"));//split each string by white space
+        System.out.println(splitString.length);//should be 5
+        //for loop to print each word
+        for(String string : splitString){
+            System.out.println(string);//print each string
+        }
+        //replace all whitespace with tabs
+        System.out.println(EXAMPLE_TEST.replaceAll("\\s+", "\t"));
+    }
+}
