d589ba7ca3ce344998864422ee0bd41a88aa30a9
Air-ic <epineda773@protonmail.com>
2019-05-14 03:42:10 -0500

    Encapsulation

---
+++ src/com.airic.practice/Encapsulation.java
@@ -0,0 +1,36 @@
+package com.airic.practice;
+
+public class Encapsulation {
+    private  String name; //name
+    private  String idNum; //idNum
+    private  int age; //age
+
+
+    //Getter and Setter methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdNum() {
+        return idNum;
+    }
+
+    public void setIdNum(String idNum) {
+        this.idNum = idNum;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+
+}
---
+++ src/com.airic.practice/EncapsulationMethod.java
@@ -0,0 +1,13 @@
+package com.airic.practice;
+
+public class EncapsulationMethod {
+    public static void main(String args[]){
+        Encapsulation c1 = new Encapsulation();
+        c1.setName("Ryan");
+        c1.setAge(26);
+        c1.setIdNum("54132");
+        System.out.println(String.format("%s with ID Number: %s is %d years old", c1.getName(), c1.getIdNum(), c1.getAge()));
+
+
+    }
+}
