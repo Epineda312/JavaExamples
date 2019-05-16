f711e82792d5a3052116605f6f3c041c182645ba
Air-ic <epineda773@protonmail.com>
2019-05-16 18:56:01 -0500

    Commented out code to avoid problems with later examples

--- src/com/ep/EarlyExamples/VehicleConstructorMethod.java
+++ src/com/ep/EarlyExamples/VehicleConstructorMethod.java
@@ -2,7 +2,7 @@ package com.ep.EarlyExamples;
 
 public class VehicleConstructorMethod {
     public static void main (String args[]){
-        Vehicle v1 = new Vehicle("Ford", "Taurus", 1992);
-        v1.start();
+        //Vehicle v1 = new Vehicle("Ford", "Taurus", 1992);
+       // v1.start();
     }
 }
