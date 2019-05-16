ae3c2ec176c023c04f8d38e0a48db0ecbfb869c1
Air-ic <epineda773@protonmail.com>
2019-05-16 15:25:14 -0500

    An example of Abstraction

---
+++ src/com.ep.practice/Abstraction/AbstractionExample.java
@@ -0,0 +1,12 @@
+package com.ep.practice.Abstraction;
+
+public class AbstractionExample {
+    public static void main(String args[]){
+        Employee e1 = new Employee();
+        e1.setEmpId("123");
+        e1.setName("Greg");
+        e1.setAddress("4175 West Acres, Los Angeles, CA, 98210");
+        System.out.println(e1.getCustomerInfo());
+
+    }
+}
